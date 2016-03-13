2177558e6e3d438864b7ded84b336fa889721cd1
Emile Filteau <emile.filteau@gmail.com>
2016-03-13 08:28:13 -0400

    Front End OP

--- minou2/static/minou2.js
+++ minou2/static/minou2.js
@@ -3,7 +3,7 @@ $(function(){
   var on_complete_analysis = function(images) {
     console.log(images);
     images.forEach(function(image) {
-      $('#image').append('<img src="'+image.preview+'"/>');
+      $('#image').append('<img src="'+image.preview+'" width="100px"/>');
     });
   }
 
